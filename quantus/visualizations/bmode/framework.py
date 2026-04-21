e82fefa9e54e6b61c14fefcbbc8b55728759856c
omid <ochaghaneh@gmail.com>
2026-04-21 13:36:13 +0200

    Initialize BmodeVisualizations framework and support for functions

--- quantus/visualizations/bmode/framework.py
+++ quantus/visualizations/bmode/framework.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import List
 from ..paramap.framework import ParamapVisualizations
+from .functions import *
 
 class_name = "BmodeVisualizations"
 
@@ -9,4 +10,8 @@ class BmodeVisualizations(ParamapVisualizations):
     Class to complete B-mode visualization (heatmaps) for bmode/radiomics results.
     Inherits from ParamapVisualizations to reuse the standard heatmap generation logic.
     """
-    pass
+    def __init__(self, analysis_obj, visualization_funcs, **kwargs):
+        super().__init__(analysis_obj, visualization_funcs, **kwargs)
+        # Use a consistent set of colormaps for B-mode features
+        self.cmaps = ["viridis", "plasma", "inferno", "magma"]
+
