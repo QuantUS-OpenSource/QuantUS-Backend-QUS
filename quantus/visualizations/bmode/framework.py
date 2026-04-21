ac6e2f0b8d13b6065a4cda9e889f3d48c5f070c9
omid <ochaghaneh@gmail.com>
2026-04-21 14:10:02 +0200

    Standardize B-mode feature names and fix visualization colormap indexing

--- quantus/visualizations/bmode/framework.py
+++ quantus/visualizations/bmode/framework.py
@@ -13,5 +13,6 @@ class BmodeVisualizations(ParamapVisualizations):
     def __init__(self, analysis_obj, visualization_funcs, **kwargs):
         super().__init__(analysis_obj, visualization_funcs, **kwargs)
         # Use colormap objects/names instead of strings to avoid 'IndexError' in draw_paramap
-        self.cmaps = [self.cmaps[0], self.cmaps[1], self.cmaps[2], self.cmaps[3]]
+        # The parent constructor already initializes self.cmaps with NumPy arrays.
+        # We just ensure we have access to them if needed, or we can just let super handle it.
 
